0dc1161ab87f2893a70969b28c07f061f613fb93
ryguy0601 <84422347+ryguy0601@users.noreply.github.com>
2025-03-04 14:38:07 -0500

    init

---
+++ src/main/java/org/jmc/pintfinder/HelloController.java
@@ -0,0 +1,14 @@
+package org.jmc.pintfinder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
