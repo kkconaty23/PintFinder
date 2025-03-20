1ff6d14d085d0059162f055dd8bf6798f38e3062
ryguy0601 <84422347+ryguy0601@users.noreply.github.com>
2025-03-20 15:06:31 -0400

    started 'create account' page and 'profile' page

--- src/main/java/org/jmc/pintfinder/Login.java
+++ src/main/java/org/jmc/pintfinder/Login.java
@@ -3,6 +3,7 @@ package org.jmc.pintfinder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ public class Login extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 475);
-        stage.setTitle("PintFinder's Login Page!");
-//        stage.getIcons().add(new Image("file:@../../../img/PintFinder_Logo.png"));
+        stage.setResizable(false);
+        stage.setTitle("PintFinder's Login Page");
+        stage.getIcons().add(new Image("file:src/main/resources/img/PintFinder_Logo.png"));
         stage.setScene(scene);
         stage.show();
     }
