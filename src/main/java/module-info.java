4cacf751a90d801b49389d1403f9e6d955e11b3a
kevin <conakp@farmingdale.edu>
2025-03-22 13:25:56 -0400

    implemented firebase login system

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,11 @@ module org.jmc.pintfinder {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires firebase.admin;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires com.google.api.apicommon;
+    requires com.google.auth;
 
     opens org.jmc.pintfinder to javafx.fxml;
     exports org.jmc.pintfinder;
--- src/main/java/org/jmc/pintfinder/LoginController.java
+++ src/main/java/org/jmc/pintfinder/LoginController.java
@@ -1,14 +1,103 @@
 package org.jmc.pintfinder;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    @FXML
+    public Button createAcctBtn;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField emailID;
+
+    @FXML
+    private Button forgotBtn;
+
+    @FXML
+    private PasswordField passwordID;
+
+    @FXML
+    private Button signInBtn;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Label warningLabel;
+
+
+    /**
+     * added a create account button allows a new window to open where the user can be added to the firebase
+     *
+     * @param event
+     */
+    @FXML
+    private void createAcctBtnClick(ActionEvent event) {//when a user creates account it makes them account that links to FB
+
+        String email = emailID.getText();
+        String password = passwordID.getText();
+
+
+        if(! checkBox.isSelected()){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            warningLabel.setText("Must Answer");
+            return;
+        }
+
+
+
+        // create a user in Firebase
+        try {
+            UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                    .setEmail(email)
+                    .setPassword(password);
+
+            UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
+            System.out.println("User created successfully: " + userRecord.getUid());//testing users
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error creating user: " + e.getMessage());
+        }
     }
+
+    public void signInBtnClick(ActionEvent actionEvent) {
+        //once we make the home page and profile pages allow this to send the user to their homepage.
+
+    }
+
+    /**
+     * registers the users email and password and puts them into firebase
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void makeActBtnClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("newAccount.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("Create Account");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+
+
 }
+
+
+
+
+
