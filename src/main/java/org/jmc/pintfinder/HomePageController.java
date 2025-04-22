b6c56b7d8d234d80511840aefdd1da4d8605ed9f
kevin <conakp@farmingdale.edu>
2025-04-22 15:21:14 -0400

    ratings working on FB, need bars that sell guinness (not brewery)

--- src/main/java/org/jmc/pintfinder/HomePageController.java
+++ src/main/java/org/jmc/pintfinder/HomePageController.java
@@ -111,8 +111,9 @@ public class HomePageController {
 
     @FXML
     public void initialize() {
-        loadBars();//method to create all bar objects
+//        loadBars();//method to create all bar objects
         FirebaseBarUploader.uploadBars(barList);
+
         URL mapUrl = getClass().getResource("/map.html");
         if (mapUrl != null) {
             System.out.println("Loading: " + mapUrl.toExternalForm());
@@ -253,85 +254,6 @@ public class HomePageController {
     }
 
 
-    /**
-     * added a create account button allows a new window to open where the user can be added to the firebase
-     *
-     * @param event
-     */
-    @FXML
-    private void createAcctBtnClick(ActionEvent event) {//when a user creates account it makes them account that links to FB
-
-        String email = emailID.getText();
-        String password = passwordID.getText();
-
-
-        if (!checkBox.isSelected()) {
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            warningLabel.setText("Must Answer");
-            return;
-        }
-
-
-        // create a user in Firebase
-        try {
-            UserRecord.CreateRequest request = new UserRecord.CreateRequest()
-                    .setEmail(email)
-                    .setPassword(password);
-
-            UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
-            System.out.println("User created successfully: " + userRecord.getUid());//testing users
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error creating user: " + e.getMessage());
-        }
-    }
-
-    public void signInBtnClick(ActionEvent actionEvent) {
-        String email = emailID.getText();
-        String password = passwordID.getText();
-
-        if (email.isEmpty() || password.isEmpty()) {
-            warningLabel.setText("Please Enter Email and Password");
-            return;
-        }
-
-        if (!checkBox.isSelected()) {
-            warningLabel.setText("You must agree to continue.");
-            return;
-        }
-
-        System.out.println("Logging in user: " + email + "...");
-
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
-            Parent root = fxmlLoader.load();
-
-            Stage stage = (Stage) signInBtn.getScene().getWindow();
-            stage.setTitle("PintFinder Home");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-            warningLabel.setText("Error loading homepage.fxml");
-        }
-
-    }
-
-    /**
-     * registers the users email and password and puts them into firebase
-     *
-     * @param event
-     * @throws IOException
-     */
-    @FXML
-    void makeActBtnClick(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("newAccount.fxml"));
-        Parent root = loader.load();
-
-        Stage stage = new Stage();
-        stage.setTitle("Create Account");
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
     @FXML
     private void handleSubmitReview() {
         String text = reviewInput.getText().trim();
@@ -445,6 +367,7 @@ public class HomePageController {
 
     /**
      * method use to create bar object from all the bars on the map
+     * ONCE NEW BARS ARE ADDED WE DONT NEED TO USE THIS METHOD
      */
     private void loadBars() {
         barList.add(new Bar( "Changing Times Pub!", 40.7481, -73.4290, 0.0));
